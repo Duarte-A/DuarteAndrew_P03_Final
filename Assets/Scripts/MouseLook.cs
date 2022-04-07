e176dc2fb92f2644595eb97315bb75b8d2307919
Andrew Duarte <aad148581@gmail.com>
2022-04-06 20:29:17 -0500

    Initial Unity Project Commit
    Storing all of our project files that build out with our default Unity project

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    public Camera cam;
+    public float zoomMultiplier = 2;
+    public float defaultFov = 60;
+    public float zoomDuration = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseLook();
+
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            ZoomCamera(defaultFov / zoomMultiplier);
+        }
+        else if (cam.fieldOfView != defaultFov)
+        {
+            ZoomCamera(defaultFov);
+        }
+
+    }
+    private void mouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void ZoomCamera(float target)
+    {
+        float angle = Mathf.Abs((defaultFov / zoomMultiplier) - defaultFov);
+        cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, target, angle / zoomDuration * Time.deltaTime);
+    }
+}
